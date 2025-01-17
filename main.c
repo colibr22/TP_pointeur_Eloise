5ca9ac32285c4a21b7ca85ad8571d15be55bba24
eloise <eloise.ansoult@student.junia.com>
2025-01-17 16:54:01 +0100

    exemple 3

--- main.c
+++ main.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 
+void min_max(int tab[] , int size, int* min, int* max)
+{
+    if (size < 1)
+    {
+        return;
+    }
+    *min = tab[0];
+    *max = tab[0];
+    for (int i = 0 ; i < size ; i++)
+    {
+        if(*min > tab[i])
+        {
+            *min = tab[i];
+        }
+        if (*max > tab[i])
+        {
+            *max = tab[i];
+        }
+    }
+}
 int main()
 {
-    int a = 2 ;
-    printf("%p", &a);
-    int *p = &a ;
-    printf("%p" , p );
-    printf("%p" , &p );
-    *p = 5 ; 
-    printf("%d" , a ) ;
+    int min = 0;
+    int max = 0;
+    int tableau = {12, 2, 8, -7, 15, 2};
+    min_max(tableau, 6, &min, &max);
+    printf(" min = %d, max = %d\n", min, max);
+    return 0;
 }
