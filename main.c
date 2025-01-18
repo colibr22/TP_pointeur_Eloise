e07cbb30730264516b614c0c47fdc6ed91f3fd8b
eloise <eloise.ansoult@student.junia.com>
2025-01-18 15:49:34 +0100

    affiche options photos

--- main.c
+++ main.c
@@ -10,12 +10,21 @@ typedef struct{
     int nb_menus;
 }Application;
 
+void displayMenu(const Application* app) {
+    printf("--- %s ---\n", app->nom);
+    for (int i = 0; i < app->nb_menus; i++) {
+        printf("%d. %s\n", i + 1, app->menus[i].option);
+    }
+    printf("Choisissez une option (0 pour quitter) : ");
+}
+
 int main() {
     Menu photoMenus[] = {
         {"Regarder une photo"},
         {"Prendre une photo"}
     };
     Application photoApp = {"Photos", photoMenus, 2};
-    
+
+    displayMenu(&photoApp);
     return 0;
 }
