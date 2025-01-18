edfb22a505fe6a5b68ce44fb720a47ba9d1008c7
eloise <eloise.ansoult@student.junia.com>
2025-01-18 21:09:37 +0100

    etape 5.3

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef struct {
     const char *option;
@@ -39,7 +41,8 @@ void updateMenu(Application* app, int menuIndex, const char* newOption) {
     if (menuIndex >= 0 && menuIndex < app->nb_menus) {
         app->menus[menuIndex].option = newOption;
         printf("Option modifiée avec succès !\n");
-    } else {
+    } 
+    else {
         printf("Index invalide.\n");
     }
 }
@@ -57,6 +60,8 @@ void runPhone(Application* app1, Application* app2) {
         printf("--- Applications ---\n");
         printf("1. %s\n", app1->nom);
         printf("2. %s\n", app2->nom);
+        printf("3. Modifier le menu de '%s'\n", app1->nom);
+        printf("4. Modifier l'application '%s'\n", app2->nom);
         printf("0. Quitter\n");
         printf("Choisissez une application (0 pour quitter) : ");
         scanf("%d", &choice);
@@ -70,16 +75,47 @@ void runPhone(Application* app1, Application* app2) {
         else if (choice == 2) {
             printf("Lancement de l'application %s...\n", app2->nom);
             runApplication(app2);
-        } 
+        }
+        else if (choice == 3) {
+            int index;
+            char newOption[100];
+            printf("Modifier une option du menu %s...\n", app1->nom);
+            printf("Entrez l'index de l'option à modifier : ");
+            scanf("%d", &index);
+            getchar();
+            printf("Entrez la nouvelle option : ");
+            fgets(newOption, 100, stdin);
+            newOption[strcspn(newOption, "\n")] = '\0';
+            updateMenu(app1, index - 1, newOption);
+        }
+        else if (choice == 4) {
+            char newName[100];
+            int newMenuCount;
+            printf("Modifier tous les attributs de l'application %s...\n", app2->nom);
+            printf("Entrez le nouveau nom de l'application : ");
+            fgets(newName, 100, stdin);
+            newName[strcspn(newName, "\n")] = '\0';
+            printf("Entrez le nombre de menus : ");
+            scanf("%d", &newMenuCount);
+            getchar();
+
+            Menu* newMenus = malloc(newMenuCount * sizeof(Menu));
+            for (int i = 0; i < newMenuCount; i++){
+                char option[50];
+                printf("Entrez l'option %d : ", i + 1);
+                fgets(option, 50, stdin);
+                option[strcspn(option, "\n")] = '\0';
+                newMenus[i].option = option;
+            }
+            updateApplication(app2, newName, newMenus, newMenuCount);
+        }
         else {
-            printf("Option invalide, veuillez choisir une application correcte.\n");
+            printf("Option invalide, veuillez choisir une option correcte.\n");
         }
     }
 }
 
-
-
-int main() {
+int main(){
     Menu photoMenus[] = {
         {"Regarder une photo"},
         {"Prendre une photo"}
