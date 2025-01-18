af3d36cf38f2d9165f04461eb41d67f11aa314ed
eloise <eloise.ansoult@student.junia.com>
2025-01-18 15:45:30 +0100

    tab photo

--- main.c
+++ main.c
@@ -11,14 +11,9 @@ typedef struct{
 }Application;
 
 int main() {
-    Menu menu1 = {"Option 1"};
-    Menu menus[] = {menu1};
-    Application app = {"Mon Application", menus, 1};
-    printf("Application: %s\n", app.nom);
-    printf("Nombre de menus: %d\n", app.nb_menus);
-    for (int i = 0; i < app.nb_menus; i++) {
-        printf("Menu %d: %s\n", i + 1, app.menus[i].option);
-    }
-
+    Menu photoMenus[] = {
+        {"Regarder une photo"},
+        {"Prendre une photo"}
+    };
     return 0;
 }
